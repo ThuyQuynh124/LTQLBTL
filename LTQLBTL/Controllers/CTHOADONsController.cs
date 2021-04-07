7bb309a8688e0b7a1f88d05dad5f20ffaab658cf
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-04-07 08:49:33 +0700

    update2

---
+++ LTQLBTL/Controllers/CTHOADONsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LTQLBTL.Models;
+
+namespace LTQLBTL.Controllers
+{
+    public class CTHOADONsController : Controller
+    {
+        private LTQLDbContext db = new LTQLDbContext();
+
+        // GET: CTHOADONs
+        public ActionResult Index()
+        {
+            var cTHOADONs = db.CTHOADONs.Include(c => c.HOADON).Include(c => c.PHIEUDANGKI);
+            return View(cTHOADONs.ToList());
+        }
+
+        // GET: CTHOADONs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHOADON cTHOADON = db.CTHOADONs.Find(id);
+            if (cTHOADON == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTHOADON);
+        }
+
+        // GET: CTHOADONs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaHD = new SelectList(db.HOADONs, "MaHD", "NhanVienLap");
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV");
+            return View();
+        }
+
+        // POST: CTHOADONs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaCTHD,MaHD,MaPDK,PHONG,TienPhong,TienDV")] CTHOADON cTHOADON)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CTHOADONs.Add(cTHOADON);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaHD = new SelectList(db.HOADONs, "MaHD", "NhanVienLap", cTHOADON.MaHD);
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTHOADON.MaPDK);
+            return View(cTHOADON);
+        }
+
+        // GET: CTHOADONs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHOADON cTHOADON = db.CTHOADONs.Find(id);
+            if (cTHOADON == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaHD = new SelectList(db.HOADONs, "MaHD", "NhanVienLap", cTHOADON.MaHD);
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTHOADON.MaPDK);
+            return View(cTHOADON);
+        }
+
+        // POST: CTHOADONs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaCTHD,MaHD,MaPDK,PHONG,TienPhong,TienDV")] CTHOADON cTHOADON)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cTHOADON).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaHD = new SelectList(db.HOADONs, "MaHD", "NhanVienLap", cTHOADON.MaHD);
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTHOADON.MaPDK);
+            return View(cTHOADON);
+        }
+
+        // GET: CTHOADONs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHOADON cTHOADON = db.CTHOADONs.Find(id);
+            if (cTHOADON == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTHOADON);
+        }
+
+        // POST: CTHOADONs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CTHOADON cTHOADON = db.CTHOADONs.Find(id);
+            db.CTHOADONs.Remove(cTHOADON);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LTQLBTL/Controllers/CTPHIEUDANGKIsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LTQLBTL.Models;
+
+namespace LTQLBTL.Controllers
+{
+    public class CTPHIEUDANGKIsController : Controller
+    {
+        private LTQLDbContext db = new LTQLDbContext();
+
+        // GET: CTPHIEUDANGKIs
+        public ActionResult Index()
+        {
+            var cTPHIEUDANGKIs = db.CTPHIEUDANGKIs.Include(c => c.PHIEUDANGKI);
+            return View(cTPHIEUDANGKIs.ToList());
+        }
+
+        // GET: CTPHIEUDANGKIs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTPHIEUDANGKI cTPHIEUDANGKI = db.CTPHIEUDANGKIs.Find(id);
+            if (cTPHIEUDANGKI == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTPHIEUDANGKI);
+        }
+
+        // GET: CTPHIEUDANGKIs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV");
+            return View();
+        }
+
+        // POST: CTPHIEUDANGKIs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaCTPDK,MaPDK,MaKH,SoNguoi,NgayDen,NgayDi")] CTPHIEUDANGKI cTPHIEUDANGKI)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CTPHIEUDANGKIs.Add(cTPHIEUDANGKI);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTPHIEUDANGKI.MaPDK);
+            return View(cTPHIEUDANGKI);
+        }
+
+        // GET: CTPHIEUDANGKIs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTPHIEUDANGKI cTPHIEUDANGKI = db.CTPHIEUDANGKIs.Find(id);
+            if (cTPHIEUDANGKI == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTPHIEUDANGKI.MaPDK);
+            return View(cTPHIEUDANGKI);
+        }
+
+        // POST: CTPHIEUDANGKIs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaCTPDK,MaPDK,MaKH,SoNguoi,NgayDen,NgayDi")] CTPHIEUDANGKI cTPHIEUDANGKI)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cTPHIEUDANGKI).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaPDK = new SelectList(db.PHIEUDANGKIs, "MaPDK", "MaNV", cTPHIEUDANGKI.MaPDK);
+            return View(cTPHIEUDANGKI);
+        }
+
+        // GET: CTPHIEUDANGKIs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTPHIEUDANGKI cTPHIEUDANGKI = db.CTPHIEUDANGKIs.Find(id);
+            if (cTPHIEUDANGKI == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTPHIEUDANGKI);
+        }
+
+        // POST: CTPHIEUDANGKIs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CTPHIEUDANGKI cTPHIEUDANGKI = db.CTPHIEUDANGKIs.Find(id);
+            db.CTPHIEUDANGKIs.Remove(cTPHIEUDANGKI);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
