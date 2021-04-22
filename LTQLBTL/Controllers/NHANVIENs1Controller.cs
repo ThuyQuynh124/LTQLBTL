833bb45c94275db2dfda8a5326f7e71a19c4b43d
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-04-22 07:53:01 +0700

    update2204

---
+++ LTQLBTL/Controllers/NHANVIENs1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LTQLBTL.Models;
+
+namespace LTQLBTL.Controllers
+{
+    public class NHANVIENs1Controller : Controller
+    {
+        private LTQLDbContext db = new LTQLDbContext();
+
+        // GET: NHANVIENs1
+        public ActionResult Index()
+        {
+            return View(db.NHANVIENs.ToList());
+        }
+
+        // GET: NHANVIENs1/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: NHANVIENs1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NHANVIENs1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNV,TenNV,MaCV,DiaChi,Tuoi,SoDT")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NHANVIENs.Add(nHANVIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nHANVIEN);
+        }
+
+        // GET: NHANVIENs1/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: NHANVIENs1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNV,TenNV,MaCV,DiaChi,Tuoi,SoDT")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nHANVIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: NHANVIENs1/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: NHANVIENs1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            db.NHANVIENs.Remove(nHANVIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
