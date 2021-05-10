079024b3bbd951d9f74c64745ac1ab98c908e8b2
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-05-11 00:40:56 +0700

    update 1105

--- LTQLBTL/Controllers/NHANVIENsController.cs
+++ LTQLBTL/Controllers/NHANVIENsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -115,6 +119,106 @@ namespace LTQLBTL.Controllers
             return RedirectToAction("Index");
         }
 
+        //upload file excel
+        [HttpPost]
+        public ActionResult Import(HttpPostedFileBase file)
+        {
+            // try
+            {
+                //upload file thanh cong va file co du lieu
+                if (file.ContentLength > 0)
+                {
+                    //dat ten cho file
+                    string _FileName = "NVKETNOI.xlsx";
+                    //duong dan luu file
+                    string _path = Path.Combine(Server.MapPath("~/Uploads/Excels"), _FileName);
+                    //luu file len server
+                    file.SaveAs(_path);
+                    //doc du lieu tu file excel upload len tra ve datatable
+                    DataTable dt = ReadDataFromExcelFile(_path);
+                    //ghi du lieu tu datatable vao sql server
+                    // CopyDataByBulk(dt);
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        NHANVIEN nv = new NHANVIEN();
+                        nv.MaNV = dt.Rows[i][0].ToString();
+                        nv.TenNV = dt.Rows[i][1].ToString();
+                        nv.MaCV = dt.Rows[i][2].ToString();
+                        nv.DiaChi = dt.Rows[i][3].ToString();
+                        nv.SoDT = dt.Rows[i][3].ToString();
+                        db.NHANVIENs.Add(nv);
+                        db.SaveChanges();
+                    }
+                    return View("Index");
+                }
+                return View("Uploadfaild");
+            }
+            //catch (Exception ex)
+            //{
+            //  return View("Uploadfaild");
+            //}
+        }
+        private void CopyDataByBulk(DataTable dt)
+        {
+            //lay ket noi voi database luu trong file webconfig
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LTQLDbContext"].ConnectionString);
+            SqlBulkCopy bulkcopy = new SqlBulkCopy(con);
+            bulkcopy.DestinationTableName = "NHANVIENs";
+            bulkcopy.ColumnMappings.Add(0, "MaNV");
+            bulkcopy.ColumnMappings.Add(1, "TenNV");
+            bulkcopy.ColumnMappings.Add(2, "DiaChi");
+            bulkcopy.ColumnMappings.Add(3, "SoDT");
+            con.Open();
+            bulkcopy.WriteToServer(dt);
+            con.Close();
+        }
+        public DataTable ReadDataFromExcelFile(string filepath)
+        {
+            string connectionString = "";
+            string fileExtention = filepath.Substring(filepath.Length - 4).ToLower();
+            if (fileExtention.IndexOf("xlsx") == 0)
+            {
+                connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + filepath + ";Extended Properties=\"Excel 12.0 Xml;HDR=NO\"";
+            }
+            else if (fileExtention.IndexOf(".xlsx") == 0)
+            {
+                connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0";
+            }
+
+            // Tạo đối tượng kết nối
+            OleDbConnection oledbConn = new OleDbConnection(connectionString);
+            DataTable data = null;
+            //try
+            {
+                // Mở kết nối
+                oledbConn.Open();
+
+                // Tạo đối tượng OleDBCommand và query data từ sheet có tên "Sheet1"
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM [NHANVIENs$]", oledbConn);
+
+                // Tạo đối tượng OleDbDataAdapter để thực thi việc query lấy dữ liệu từ tập tin excel
+                OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                oleda.SelectCommand = cmd;
+
+                // Tạo đối tượng DataSet để hứng dữ liệu từ tập tin excel
+                DataSet ds = new DataSet();
+
+                // Đổ đữ liệu từ tập excel vào DataSet
+                oleda.Fill(ds);
+
+                data = ds.Tables[0];
+            }
+            //catch
+            //{
+            //}
+            //finally
+            //{
+            //    // Đóng chuỗi kết nối
+            //    oledbConn.Close();
+            //}
+            return data;
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
